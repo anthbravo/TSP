a644f01231d741c6271886c43b2cbf1e9694570b
Anthony Bravo Berrio <anthony.bravo.berrio@gmail.com>
2019-05-01 19:19:51 -0500

    Se agrega css y se muestra el mapa con los marcadores generados a partir del dataset

--- js/app.js
+++ js/app.js
@@ -1,9 +1,65 @@
-var urlDataset = '/resource/dataset.json';
+var dataset;
 
-fetch(urlDataset)
-    .then(response => response.json())
-    .then(data => {
+function obtainDataset() {
 
-        console.log(data);
+    var urlDataset = '/resource/dataset.json';
 
-    });
+    fetch(urlDataset)
+        .then(response => response.json())
+        .then(data => {
+            var codigo = 0;
+            console.log(codigo);
+            dataset = data;
+            dataset.forEach(data => {
+                data.title = 'Departamento: ' +
+                    data.DEP +
+                    ' | Provincia: ' +
+                    data.PROV +
+                    ' | Distrito: ' +
+                    data.DIST +
+                    ' | Centro Poblado: ' +
+                    data.MNOMCP;
+                data.label = codigo;
+
+                codigo++;
+            });
+        });
+}
+
+obtainDataset();
+
+window.onload = function () {
+
+    var map;
+
+    function initializeMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: {
+                lat: -9.4154388,
+                lng: -74.1862975
+            },
+            zoom: 5.7
+        });
+    }
+
+    function loadMarks() {
+        dataset.forEach(data => {
+            new google.maps.Marker({
+                position: {
+                    lat: data.YGD,
+                    lng: data.XGD
+                },
+                title: data.title,
+                label: data.label + '',
+                map: map
+            });
+        });
+    }
+
+    initializeMap();
+    loadMarks();
+
+
+
+
+}
