4fbf01165fd8dc372aeaf8eb3bfb9ba2c873ee80
Anthony Bravo Berrio <anthony.bravo.berrio@gmail.com>
2019-05-04 14:03:42 -0500

    Se implementa el calculo de camino mas corto para el tsp, se modifica dataset, se agrega libreria geometry y se implementa trazado de lineas por el camino encontrado

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 var dataset;
+var optimumTravel = [];
+var maximumPermutations;
+var countPermutations = 0;
 
 function obtainDataset() {
 
@@ -25,7 +28,16 @@ function obtainDataset() {
                     '</br>' +
                     '<strong>Longitud: </strong>' + data.XGD;
             });
+            maximumPermutations = calculatePermutation(data.length - 1) / 2;
         });
+}
+
+function calculatePermutation(value) {
+    if (value <= 2) {
+        return value;
+    } else {
+        return value * calculatePermutation(value - 1);
+    }
 }
 
 obtainDataset();
@@ -76,10 +88,92 @@ window.onload = function () {
 
                 infoWindow.close();
 
-                console.log(data.index);
+                var travel = [];
+
+                tsp(dataset, data.index, travel);
+
+                console.log(optimumTravel);
+
+                var flightPath = new google.maps.Polyline({
+                    path: optimumTravel,
+                    geodesic: true,
+                    strokeColor: '#FF0000',
+                    strokeOpacity: 1.0,
+                    strokeWeight: 1
+                });
+
+                flightPath.setMap(map);
             });
 
         });
+    }
+
+    function tsp(dataset, index, travel) {
+
+        let data = JSON.parse(JSON.stringify(dataset));
+        let newTravel = JSON.parse(JSON.stringify(travel));
+
+        if (countPermutations <= maximumPermutations) {
+
+            if (data.length == 0) {
+
+                var point = new Object();
+                point.name = newTravel[0].name;
+                point.lat = newTravel[0].lat;
+                point.lng = newTravel[0].lng;
+
+                if (newTravel.length > 0) {
+                    point.distance =
+                        google.maps.geometry.spherical.computeDistanceBetween(
+                            new google.maps.LatLng(point.lat, point.lng),
+                            new google.maps.LatLng(newTravel[newTravel.length - 1].lat, newTravel[newTravel.length - 1].lng));
+                    point.total = newTravel[newTravel.length - 1].total + point.distance;
+                }
+
+                newTravel.push(point);
+
+                if (optimumTravel.length == 0) {
+                    optimumTravel = newTravel;
+                } else if (newTravel[newTravel.length - 1].total <
+                    optimumTravel[optimumTravel.length - 1].total) {
+                    optimumTravel = newTravel;
+                }
+
+                countPermutations++;
+
+            } else {
+
+                var point = new Object();
+                point.name = data[index].MNOMCP;
+                point.lat = data[index].YGD;
+                point.lng = data[index].XGD;
+
+                if (newTravel.length == 0) {
+                    point.distance = 0;
+                    point.total = 0;
+                } else {
+                    point.distance =
+                        google.maps.geometry.spherical.computeDistanceBetween(
+                            new google.maps.LatLng(data[index].YGD, data[index].XGD),
+                            new google.maps.LatLng(newTravel[newTravel.length - 1].lat, newTravel[newTravel.length - 1].lng));
+                    point.total = newTravel[newTravel.length - 1].total + point.distance;
+                }
+                data.splice(index, 1);
+                newTravel.push(point);
+
+                if (data.length == 0) {
+                    tsp(data, -1, newTravel);
+                } else {
+                    for (let i = 0; i < data.length; i++) {
+                        tsp(data, i, newTravel);
+                    }
+
+                }
+
+            }
+
+        }
+
     }
 
     initializeMap();
