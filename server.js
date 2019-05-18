46e34b7df5f602f7f6f99d9a8219530294c850e1
Anthony Joaquin Bravo Berrio <abravo@hiper.com.pe>
2019-05-18 17:26:35 -0500

    test

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-const hostname = 'localhost';
 const port = process.env.PORT || 5000;
 
 const server = require('./controller.js');
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
 });
