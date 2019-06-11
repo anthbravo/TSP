16887a0176f55092bebb0b066d7fea85d613552c
Anthony Bravo Berrio <anthony.bravo.berrio@gmail.com>
2019-06-10 23:24:31 -0500

    Se reformula la generacion de los recursos para centros poblados

--- resource/CP.js
+++ resource/CP.js
@@ -1,26 +1,41 @@
 var fs = require('fs');
 
+function loadAndWriteCP(INPUT, OUTPUT, CAPITAL_CODE) {
 
-function loadAndWriteCP(CAPITAL_CODE){
-    var dataset = fs.readFileSync('resource/CP/dataset.json');
+    var dataset = fs.readFileSync(INPUT);
     var newDataset = new Array();
 
-    centroPoblado = JSON.parse(dataset);
+    centrosPoblados = JSON.parse(dataset);
 
-    centroPoblado.forEach(CP => {
+    centrosPoblados.forEach(CP => {
 
-        if(CP.CAPITAL == CAPITAL_CODE){
-            newDataset.push(CP);
+        if (CP.CAPITAL == CAPITAL_CODE) {
+            var centroPoblado = {};
+
+            centroPoblado['code'] = CP.CODCP;
+            centroPoblado['name'] = CP.NOMCP;
+            centroPoblado['lat'] = CP.YGD;
+            centroPoblado['long'] = CP.XGD;
+
+            newDataset.push(centroPoblado);
         }
-        
+
     });
 
-    fs.writeFileSync('resource/CP/CP_DISTRITOS.json', JSON.stringify(newDataset), 'utf8');
+    newDataset.sort((a, b) => {
+        return a['lat'] - b[['lat']];
+    });
+
+    fs.writeFileSync(OUTPUT, JSON.stringify(newDataset), 'utf8');
 }
+
 /**
  * 0 RESTANTES
  * 1 CAPITALES
  * 2 PROVINCIAS
  * 3 DISTRITOS
  */
-loadAndWriteCP("3");
+var INPUT = 'resource/CP/dataset.json';
+var OUTPUT = 'resource/CP/CP_CAPITALES.json';
+var CAPITAL_CODE = '1';
+loadAndWriteCP(INPUT, OUTPUT, CAPITAL_CODE);
